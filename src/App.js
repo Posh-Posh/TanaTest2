a5fe36b88fe34ab8b7bb5755d0ee60bed6021387
Posh-Posh <pauline_fremont@live.fr>
2020-01-29 16:01:35 +0100

    full exercise

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App({data}) {
+  
+  const list = data.map( (info) => {
+
+    const contentKeys = Object.keys(info.ressources);
+    const elements = contentKeys.map((t) => 
+      info.ressources[t].map((e) => (<li key={t}>{t} : <span className="version">{e}</span>,</li>))
+    );
+    
+  return(elements);
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Résultats détaillés</h1>   
+        {data.map( (info, i) => {
+
+          return (
+            <main key={i}>
+              <h2><span>1</span> - {info.name}</h2>
+              <p className="explanation">{info.explanation}</p>                 
+
+              <ul className="ressources">
+                {list}
+              </ul> 
+
+              {info.data.map( (elem, j) => {
+
+                return (
+                  <section key={j}>
+                    <div className="rectangle">
+                      <p>140 x 100</p>
+                    </div>
+
+                    <div className="frame">
+                      <div className="header">
+                        <p className="outer">{elem.outer}</p>
+                        <div className="circle"></div>
+                      </div>
+                      <h3>Détails</h3>
+                      <h4>Attributs ARIA : </h4>
+                      <ul className="aria">
+                        {elem.accessibleName == null ? (
+                          <li>Nom accessible : <span className="detail">Non spécifié</span></li>
+                        ) : (
+                          <li>Nom accessible : <span className="detail">{elem.accessibleName}</span></li>
+                        )}                      
+                        
+                        {elem.canBeReachedUsingKeyboardWith.length === 0 ? (
+                          <li>Atteignable au clavier :<span className="detail"> Non</span></li>
+                        ) : (
+                          <li>Atteignable au clavier : <span className="detail">Oui</span></li>
+                        )}
+
+                        {elem.isNotVisibleDueTo = [] ? (
+                          <li>Affiché : <span className="detail">Oui</span></li>
+                        ) : (
+                          <li>Affiché : <span className="detail">{elem.isNotVisibleDueTo}</span></li>
+                        )}   
+                      </ul>
+                    </div>
+                  </section>
+                )
+              })}              
+            </main>
+          )
+        })
+      }        
     </div>
   );
 }
